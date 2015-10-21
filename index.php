58e88b7f228aa407e8e73af395267a40840c710a
Luke Anderson <stuntguy3000@gmail.com>
2015-10-22 00:56:52 +1030

    Inital Commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+		
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/style.css">
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		
+		<title>stuntguy3000 | Homepage</title>
+	</head>
+	<body>
+		<!-- <nav class="navbar navbar-default navbar-static-top" style="background: #fff; border: 0;">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <a class="navbar-brand" style="padding: 0">
+				<img alt="stuntguy300.me" src="img/avatar.png" width="50" height="50">
+			  </a>
+			</div>
+		  </div>
+		</nav> -->
+		<div class="logo">
+			<img alt="stuntguy300.me" src="img/avatar.png" width="50" height="50">
+		</div>
+		<div class="logoTitle">stuntguy3000</div>
+		<div class="menu"><a href="./">Home</a>&nbsp;&nbsp;<a href="./">About Me</a>&nbsp;&nbsp;<a href="./">Blog</a></div>
+		<div class="background">
+			<div class="caption">
+				<div class="title">Adelaide Oval Footbridge & River Torrens</div>The Adelaide Oval Footbridge, an iconic tourist location spanning over the River Torrens towards Adelaide Oval.<br>
+			</div>
+			<a class="copyright" target="_blank" href="https://www.flickr.com/photos/stuntguy3000/21575594581/in/dateposted-public/">Photographed by stuntguy3000<br>© Some Rights Reserved</a>
+		</div>
+	</body>
+</html>
