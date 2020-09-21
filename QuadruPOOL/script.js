d39959cc6341eb93770bb12ec87ce298319a0387
Luke Anderson <stuntguy3000@gmail.com>
2020-09-21 20:59:07 +0930

    Add QuadruPOOL scorecard

---
+++ QuadruPOOL/script.js
@@ -0,0 +1,12 @@
+function addScore(colour) {
+    console.log("Adding score to " + colour);
+
+    const element = document.getElementById(colour + "-score");
+    const score = parseInt(element.innerHTML);
+
+    console.log("Old score: " + score);
+
+    element.innerHTML = score + 1;
+
+    console.log("New score: " + element.innerHTML);
+}
