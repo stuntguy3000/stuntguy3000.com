d072dae560394600f8ad4a779e06434f187df6db
Luke Anderson <stuntguy3000@gmail.com>
2020-09-29 19:16:06 +0930

    Compress assets and modify css to hopefully support fallback font sizes

--- QuadruPOOL/script.js
+++ QuadruPOOL/script.js
@@ -1,16 +1,13 @@
-function addScore(colour) {
-    console.log("Adding score to " + colour);
+var total_score = 0;
 
+function addScore(colour) {
     const element = document.getElementById(colour + "-score");
     const score = parseInt(element.innerHTML);
 
-    console.log("Old score: " + score);
-
     element.innerHTML = score + 1;
+	total_score += 1;
 
-    console.log("New score: " + element.innerHTML);
-
-    // Play sound effect
+    // Play ding sound effect
     var audio = new Audio('ding.mp3');
     audio.play();
 }
